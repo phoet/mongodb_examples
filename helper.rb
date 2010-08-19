01ec324a1d3f56dbe9d46ef6c079976e2ca767c6
phoet <phoetmail@googlemail.com>
2010-08-19 12:42:21 +0200

    use apigee

--- helper.rb
+++ helper.rb
@@ -4,7 +4,7 @@ require 'crack/json'
 module Helper
 
   def load_post_from_twitter(post_id)
-    resp = HTTPClient.new.get_content "http://api.twitter.com/1/statuses/show/#{post_id}.json"
+    resp = HTTPClient.new.get_content "http://#{ENV['APIGEE_TWITTER_API_ENDPOINT']}/1/statuses/show/#{post_id}.json"
     Crack::JSON.parse resp
   end
   
