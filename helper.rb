fd342d54a51e3e1d4d8a196be280272aec1d0cf3
phoet <phoetmail@googlemail.com>
2010-08-19 13:00:31 +0200

    catch bad ids

--- helper.rb
+++ helper.rb
@@ -6,6 +6,8 @@ module Helper
   def load_post_from_twitter(post_id)
     resp = HTTPClient.new.get_content "http://#{ENV['APIGEE_TWITTER_API_ENDPOINT']}/1/statuses/show/#{post_id}.json"
     Crack::JSON.parse resp
+  rescue
+    {"error" => "an error occured loading tweet #{post_id} (#{$!})"}
   end
   
 end
